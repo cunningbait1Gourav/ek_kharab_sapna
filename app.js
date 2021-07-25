38603f3a1bbf65edbb7db0747dd99c60d6c54739
Gourav sonkar <79933807+cunningbait1Gourav@users.noreply.github.com>
2021-07-25 12:38:05 +0530

    v1.0.0.0

---
+++ app.js
@@ -0,0 +1,168 @@
+class Products
+{
+	constructor(
+		name,
+		no,
+		desc,
+		feat1,
+		feat2,
+		feat3,
+		feat4,
+		rate1,
+		rate2,
+		rate3,
+		)
+	{
+		this.name=name;
+		this.no=no;
+		this.desc=desc;
+		this.feat1=feat1;
+		this.feat2=feat2;
+		this.feat3=feat3;
+		this.feat4=feat4;
+		this.rate1=rate1;
+		this.rate2=rate2;
+		this.rate3=rate3;
+		
+	}
+}
+
+const prod1=new Products(
+	name="Mac book 13'3",
+	no="1",
+	desc="2019 Apple Mac Book Pro<br> (16-inch/40.65cm ,16 GB RAM , 1TB SSD, Internal storage 2TB<br>Price:Rs 2,13,999<br><br><br>",
+	feat1="16 GB Dynamic RAM",
+	feat2="1TB SSD",
+	feat3="Octa-core processor",
+	feat4="13'3 Super amoled display",
+	rate1= "60%",
+	rate2= "50%",
+	rate3= "56%",
+)
+
+const prod2=new Products(
+	name="APPLE iPhone 12 Pro Max",
+	no="2",
+	desc="Apple iphone 12 pro max<br>128 GB ROM,17.02cm,12mp+12mp+12mp |12mp front camera,A14 bionic chip <br>Price:1,25,900<br><br><br>",
+	feat1="128 GB ROM",
+	feat2="17.02cm screen width Super amoled display",
+	feat3="12mp+12mp+12mp",
+	feat4="A14 bionic chip",
+	rate1= "80%",
+	rate2= "70%",
+	rate3= "75%",
+)
+const prod3=new Products(
+	name="Sareee",
+	no="3",
+	desc="Indian saree,solid fashion<br>synthetic saree(rose pink,bottle green,black,blue)<br>Price:6,499<br><br><br>",
+	feat1="PURE Rayon",
+	feat2="Banarasi",
+	feat3="Available in different patterns",
+	feat4="Emobordiered",
+	rate1= "50%",
+	rate2= "70%",
+	rate3= "60%",
+)
+const prod4=new Products(
+	name="ADIDAS",
+	no="4",
+	desc="Genuine products of adidas<br>India Marketing Private Ltd.<h3>Both for men and women</h3><br>Price:2,157<br><br><br>",
+	feat1="Comfortable footware",
+	feat2="Adidas Genuine",
+	feat3="Available in different colors",
+	feat4="unisex",
+	rate1= "60%",
+	rate2= "90%",
+	rate3= "75%",
+)
+const prod5=new Products(
+	name="Sling Bag",
+	no="5",
+	desc="WOMEN MARKS<br>Girls & women sling bag<br>Price:9,999<br><br><br>",
+	feat1="Adjustale slings",
+	feat2="Rexin body/no leather used",
+	feat3="Available in different colors",
+	feat4="3 different sizes available",
+	rate1= "70%",
+	rate2= "40%",
+	rate3= "55%",
+)
+const prod6=new Products(
+	name="APPLE Airpods",
+	no="6",
+	desc="APPLE Airpods Pro with wireless charging case<br>Active noise cancellation enabled Bluetooth Headset(White,True wireless)<br>Price:20,999<br><br><br>",
+	feat1="Wireless charging",
+	feat2="Designed to reduce noise",
+	feat3="Available in 2 colours",
+	feat4="Lightweight easy to carry",
+	rate1= "75%",
+	rate2= "85%",
+	rate3= "80%",
+)
+const prod7=new Products(
+	name="Skybag",
+	no="7",
+	desc="SKYBAGS BRAT 10 BACK TO SCHOOL<br>SCHOOL BAG(E)LT. BLUE 25L Backpack<br>Price:1,549<br><br><br>",
+	feat1="WARRENTY 1YR",
+	feat2="Available in two colors",
+	feat3="Best Quality Cloth",
+	feat4="Lightweight easy to carry",
+	rate1= "95%",
+	rate2= "85%",
+	rate3= "90%",
+)
+const prod8=new Products(
+	name="mCaffeine",
+	no="8",
+	desc="Naked and Raw Coffee Face Wash,100ml<h3>Give your face the daily boost of clear freshness withmCaffiene Naked & Raw Coffee Face Wash with a deep cleansing formula<h3><br>Price:Rs 524<br><br><br>",
+	feat1="Clears acne and pimples",
+	feat2="Pollution cleaner",
+	feat3="Available in 4 different coffee flavours",
+	feat4="Brightens face",
+	rate1= "65%",
+	rate2= "55%",
+	rate3= "60%",
+)
+const prod9=new Products(
+	name="SHIRT",
+	no="9",
+	desc="Mens casual wear shirt,brown and black slim fit<br>Price:Rs 1,127<br><br><br>",
+	feat1="Available in all sizes",
+	feat2="Avaiable in two colours",
+	feat3="Best quality cloth",
+	feat4="12 days Return policy",
+	rate1= "85%",
+	rate2= "75%",
+	rate3= "80%",
+)
+const prod10=new Products(
+	name="SASSAFRAS",
+	no="10",
+	desc="Women Sheath pink dress<br>Price:Rs 14,545<br><br><br>",
+	feat1="Available in all sizes",
+	feat2="Avaiable in sasy pink colour",
+	feat3="Best quality cloth",
+	feat4="12 days Return policy",
+	rate1= "85%",
+	rate2= "75%",
+	rate3= "80%",
+)
+const prod11=new Products(
+	name="APPLE Watch",
+	no="11",
+	desc="Apple Watch Series 6 GPS 44mm <br>Blue aluminium Case with Deep Navy Sport Band(Blue Strap,Regular)<br>Price:Rs 43,000<br><br><br>",
+	feat1="GPS enabled",
+	feat2="Avaiable in 4 colours",
+	feat3="size 44mm",
+	feat4="1 year WARRENTY",
+	rate1= "95%",
+	rate2= "55%",
+	rate3= "75%",
+)
+
+
+
+
+
+
